6cd839a1f6f1764080ba1cfacda54dd1a1d3a4c9
wlqmfl <jiseung.hong@kaist.ac.kr>
2024-12-09 21:14:58 -0500

    Fix minor parallelism

--- openhands_resolver/resolve_issues.py
+++ openhands_resolver/resolve_issues.py
@@ -380,8 +380,9 @@ async def process_issue(
     return output
 
 # This function tracks the progress AND write the output to a JSONL file
-async def update_progress(output: Awaitable[ResolverOutput], output_fp: TextIO, pbar: tqdm) -> None:
-    resolved_output = await output
+async def update_progress(output: ResolverOutput, output_fp: TextIO, pbar: tqdm) -> None:
+    # output is now ResolverOutput, not Awaitable[ResolverOutput]
+    resolved_output = output
     pbar.update(1)
     pbar.set_description(f'issue {resolved_output.issue.number}')
     pbar.set_postfix_str(
